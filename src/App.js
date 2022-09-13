ad60a78af7b4485fa4ac4691d7675da088ef2e84
Wookyung <namwookyung.dev@gmail.com>
2022-09-14 01:14:51 +0900

    리액트 라우터로 SPA 개발 학습중_22.09.14

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" component={<Home/>} />
+        <Route path="/about" component={<About/>} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
